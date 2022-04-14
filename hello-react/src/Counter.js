7eed8afa5310d234dea988190c8213ed8e3745bf
bomin-1206 <gbm4655@naver.com>
2022-04-14 18:06:04 +0900

    Counter.js

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,24 +1,32 @@
 import { Component } from 'react';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    // state의 초깃값 설정하기
-    this.state = {
-      number: 0,
-    };
-  }
+  state = {
+    number: 0,
+    fixedNubmer: 0,
+  };
   render() {
-    const { number } = this.state; //state를 조회할 때는 this.state로 조회합니다.
-    // 클래스형 컴포넌트가 지니고 있는 state, 함수 컴포넌트에서는 useState를 사용
+    const { number, fixedNubmer } = this.state; //state를 조회할 때는 this.state로 조회합니다.
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: {fixedNubmer}</h2>
         <button
           //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
           onClick={() => {
-            //this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-            this.setState({ number: number + 1 });
+            // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+            // 위 코드와 아래 코드는 완전 히 똑같은 기능을 합니다.
+            // 아래 코드는 함수에서 바로 객체를 반환한다는 의미입니다.
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
+            // this.setState({number: number+1});
+            // this.setState({number: this.state.number+1});
           }}
         >
           +1
@@ -29,3 +37,68 @@ class Counter extends Component {
 }
 
 export default Counter;
+
+// import { Component } from 'react';
+
+// class Counter extends Component {
+//     constructor(props) {
+//         super(props);
+//         // state의 초깃값 설정하기
+//         this.state = {
+//             number: 0,
+//             fixedNumber: 0
+//         };
+//     }
+//     render() {
+//         const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회합니다.
+//         return (
+//             <div>
+//                 <h1>{number}</h1>
+//                 <h2>바뀌지 않는 값: {fixedNumber}</h2>
+//                 <button
+//                     // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
+//                     onClick={() => {
+//                         // this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
+//                         this.setState({ number: number+1});
+//                     }}
+//                 >
+//                     +1
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Counter;
+
+// import { Component } from 'react';
+
+// class Counter extends Component {
+//   constructor(props) {
+//     super(props); // 클래스형 컴포넌틍서 constructor를 작성할 때는 반드시 super(props)를 호출해 주어야 한다.
+//     // state의 초깃값 설정하기
+//     this.state = {
+//       number: 0,
+//     };
+//   }
+//   render() {
+//     const { number } = this.state; //state를 조회할 때는 this.state로 조회합니다.
+//     // 클래스형 컴포넌트가 지니고 있는 state, 함수 컴포넌트에서는 useState를 사용
+//     return (
+//       <div>
+//         <h1>{number}</h1>
+//         <button
+//           //onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
+//           onClick={() => {
+//             //this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
+//             this.setState({ number: number + 1 });
+//           }}
+//         >
+//           +1
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
