a7e923a4190dc3c018a1fe652d837ee5f5d9babb
bomin-1206 <gbm4655@naver.com>
2022-05-03 21:01:11 +0900

    EventPractice.js

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -2,18 +2,20 @@ import { Component } from 'react';
 
 class EventPractice extends Component {
   state = {
-    message: '',
+    username: '',
+    massage: '',
   };
 
   handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
   handleClick = () => {
-    alert(this.state.message);
+    alert(this.state.username + ': ' + this.state.message);
     this.setState({
+      username: '',
       message: '',
     });
   };
@@ -22,6 +24,13 @@ class EventPractice extends Component {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.usernmae}
+          onChange={this.handleChange}
+        />
         <input
           type="text"
           name="message"
@@ -36,6 +45,45 @@ class EventPractice extends Component {
 }
 
 export default EventPractice;
+
+// import { Component } from 'react';
+
+// class EventPractice extends Component {
+//   state = {
+//     message: '',
+//   };
+
+//   handleChange = (e) => {
+//     this.setState({
+//       message: e.target.value,
+//     });
+//   };
+
+//   handleClick = () => {
+//     alert(this.state.message);
+//     this.setState({
+//       message: '',
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>이벤트 연습</h1>
+//         <input
+//           type="text"
+//           name="message"
+//           placeholder="아무거나 입력해 보세요"
+//           value={this.state.message}
+//           onChange={this.handleChange}
+//         />
+//         <button onClick={this.handleClick}>확인</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default EventPractice;
 
 // import { Component } from 'react';
 
