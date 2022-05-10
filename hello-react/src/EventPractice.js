07e86804af4fbb6101783a0b437f80d6eedc3b25
bomin-1206 <gbm4655@naver.com>
2022-05-10 21:05:03 +0900

    EventPractice,js

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,18 +1,28 @@
-import { useReducer, useState } from 'react';
+import { useState } from 'react';
 
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, //기존의 form 내용을 이 자리에 복사한 뒤
+      [e.target.name]: e.target.value, //원하는 값을 덮어 씌우기
+    };
+    setForm(nextForm);
+  };
   const onClick = () => {
     alert(username + ': ' + message);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: '',
+    });
   };
   const onKeyPress = (e) => {
     if (e.key === 'Enter') {
-      onClick();
+      onclick();
     }
   };
   return (
@@ -23,15 +33,15 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
-        onKetPress={onKeyPress}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
     </div>
@@ -39,6 +49,48 @@ const EventPractice = () => {
 };
 
 export default EventPractice;
+
+// import { useState } from 'react';
+
+// const EventPractice = () => {
+//   const [username, setUsername] = useState('');
+//   const [message, setMessage] = useState('');
+//   const onChangeUsername = (e) => setUsername(e.target.value);
+//   const onChangeMessage = (e) => setMessage(e.target.value);
+//   const onClick = () => {
+//     alert(username + ': ' + message);
+//     setUsername('');
+//     setMessage('');
+//   };
+//   const onKeyPress = (e) => {
+//     if (e.key === 'Enter') {
+//       onClick();
+//     }
+//   };
+//   return (
+//     <div>
+//       <h1>이벤트 연습</h1>
+//       <input
+//         type="text"
+//         name="username"
+//         placeholder="사용자명"
+//         value={username}
+//         onChange={onChangeUsername}
+//       />
+//       <input
+//         type="text"
+//         name="message"
+//         placeholder="아무거나 입력해 보세요"
+//         value={message}
+//         onChange={onChangeMessage}
+//         onKetPress={onKeyPress}
+//       />
+//       <button onClick={onClick}>확인</button>
+//     </div>
+//   );
+// };
+
+// export default EventPractice;
 
 // import { Component } from 'react';
 
