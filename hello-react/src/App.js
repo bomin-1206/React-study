17e337d5b80fd20905482ce03030df63037676bc
bomin-1206 <gbm4655@naver.com>
2022-05-26 16:56:17 +0900

    React-0526

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,20 +1,31 @@
 import { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
 export default App;
+
+// import { Component } from 'react';
+// import ScrollBox from './ScrollBox';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+//         <button onClick={() => this.scrollBox.scrollToBottom()}>
+//           맨 밑으로
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 
 // import { Component } from 'react';
 // import ScrollBox from './ScrollBox';
