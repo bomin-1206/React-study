bf44662c8876ec02a7d584fc3a0f2ef1b07953ff
bomin-1206 <gbm4655@naver.com>
2022-12-02 14:38:24 +0900

    React_blog-1202

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,8 +1,6 @@
 /* eslint-disable */ // WARNING 메세지 없애기
-
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
   let post = "강남 우동 맛집"; // let, var, const
@@ -18,6 +16,8 @@ function App() {
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   // let [modal, setModal] = useState(0); // 내가 한 방법
   let [modal, setModal] = useState(false); // 강의에서 한 방법
+  let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState("");
 
   return (
     <div className="App">
@@ -81,12 +81,17 @@ function App() {
             <h4
               onClick={() => {
                 setModal(!modal);
+                setTitle(i);
               }}
             >
               {e}
               <span
-                onClick={() => {
-                  따봉변경(따봉 + 1);
+                onClick={(e) => {
+                  e.stopPropagation(); // 이게 없으면 좋아요를 누르면 상위에 있는 onClick이 실행이 되면서 모달창이 열리는데
+                  // 이 한줄을 통해 모달창이 열리는 것을 막는다.
+                  let good = [...따봉];
+                  good[i] = good[i] + 1;
+                  따봉변경(good);
                 }}
               >
                 ❤️
@@ -95,23 +100,82 @@ function App() {
             </h4>
             {/* <h4>{글제목[i]}</h4> */}
             <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+              }}
+            >
+              삭제
+            </button>
           </div>
         );
       })}
+      <input
+        onChange={(e) => {
+          입력값변경(e.target.value);
+          console.log(입력값);
+        }}
+      />
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.push(입력값);
+          글제목변경(copy);
+        }}
+      >
+        글발행
+      </button>
       {/*{modal % 2 == 0 ? null : <Modal />}*/} {/* 내가 한 방법 */}
-      {modal == true ? <Modal /> : null} {/* 강의에서 한 방법 */}
+      {modal == true ? (
+        <Modal title={title} 글제목변경={글제목변경} 글제목={글제목} />
+      ) : null}
+      <Modal2 />
+      {/* 강의에서 한 방법 */}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button
+        onClick={() => {
+          props.글제목변경(["여자코트 추천", "강남 우동맛집", "파이썬 독학"]);
+        }}
+      >
+        글 수정
+      </button>
     </div>
   );
+}
+
+class Modal2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "kim",
+      age: "20",
+    };
+  }
+  render() {
+    return (
+      <div>
+        안녕 {this.state.age}
+        <button
+          onClick={() => {
+            this.setState({ age: 21 });
+          }}
+        >
+          버튼
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
