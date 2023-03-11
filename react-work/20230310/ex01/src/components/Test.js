1aa06c02d4c87cd7176d7f9513aad7fa044e4b7a
bomin-1206 <gbm4655@naver.com>
2023-03-11 23:46:43 +0900

    React_school_study-0311

---
+++ react-work/20230310/ex01/src/components/Test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Test = () => {
+    const arr = [10,20];
+    const objs = {a:"aa", b:"bb"};
+
+    const doA = () => {return "doA";};
+    const doB = () => {return "doB";};
+
+    arr[0] = 30;
+    objs.a = "aaa"
+
+    return (
+        <div>
+            Test
+            <h3>{arr[0]}</h3>
+            <h3>{objs.a}</h3>
+            <h3>{doA()}</h3>
+            <h3>{doB()}</h3>
+        </div>
+    );
+}
+ 
+export default Test;
