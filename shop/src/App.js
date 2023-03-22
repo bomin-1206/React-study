752f5b9bd28deb2c832286eeb220a101b402ab31
bomin-1206 <gbm4655@naver.com>
2023-03-22 21:51:34 +0900

    React_Route,styled-component - 0322

--- shop/src/App.js
+++ shop/src/App.js
@@ -7,12 +7,15 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useState } from "react";
 import data from './data';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from "./routes/Detail";
 
 // import bg from './img/bg.png';
 function App() {
 
   let [shoes] = useState(data);
-  let [image] = useState(['https://codingapple1.github.io/shop/shoes1.jpg', 'https://codingapple1.github.io/shop/shoes2.jpg', 'https://codingapple1.github.io/shop/shoes3.jpg'])
+  let [image] = useState(['https://codingapple1.github.io/shop/shoes1.jpg', 'https://codingapple1.github.io/shop/shoes2.jpg', 'https://codingapple1.github.io/shop/shoes3.jpg']);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -20,24 +23,53 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+            {/* <Nav.Link onClick={() => { navigate(1) }}>Home</Nav.Link> {/* 앞으로 한 페이지 이동 */}
+            {/* <Nav.Link onClick={() => { navigate(-1) }}>Home</Nav.Link> 뒤로 한 페이지 이동 */}
+            <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      {/* <div className='main-bg' style={{backgroundImage : 'url(' + bg + ')'}}></div> */}
-      <div className="main-bg"></div>
-      <Container>
-        <Row>
-          {
-            shoes.map(function(e, i) {
-              return (
-                <Card shoes={shoes} image={image} i={i}/>
-              )
-            })
-          }
-        </Row>
-    </Container>
+
+      
+      {/* <Link style={{padding: '10px', textDecoration: 'none', color : 'black' }} to="/">홈</Link>
+      <Link style={{padding: '10px', textDecoration: 'none', color : 'black' }} to="/detail">상세페이지</Link> */}
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            {/* <div className='main-bg' style={{backgroundImage : 'url(' + bg + ')'}}></div> */}
+            <div className="main-bg"></div>
+            <Container>
+              <Row>
+                {
+                  shoes.map(function(e, i) {
+                    return (
+                      <Card shoes={shoes} image={image} i={i}/>
+                    )
+                  })
+                }
+              </Row>
+          </Container>
+        </>
+        } />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<p>멤버임</p>} />
+          <Route path="location" element={<p>위치정보임</p>} />
+        </Route>
+
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
+        </Route>
+
+        {/* <Route path="/about/member" element={<About />} />
+        <Route path="/about/location" element={<About />} /> */}
+
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
     </div>
   );
 }
@@ -50,6 +82,24 @@ function Card(props) {
       <h4>{props.shoes[props.i].title}</h4>
       <p>{props.shoes[props.i].content}</p>
     </Col>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
   )
 }
 
---
+++ shop/src/routes/Detail.js
@@ -0,0 +1,50 @@
+import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+// let Btn = styled.button`
+//   background: ${ props => props.bg };
+//   color: ${ props => props.bg == 'blue' ? 'white' : 'black' };
+//   padding: 10px;
+`
+// let NewBtn = styled.button(Btn)``
+
+// let BlackBox = styled.div`
+//   background: grey;
+//   padding: 20px;
+// `
+
+function Detail(props) {
+
+  let {id} = useParams();
+  let 찾은상품 = props.shoes.find(function(x) {
+    return x.id == id
+  });
+
+  return (
+    <div className="container">
+
+      {/* <Btn bg="blue">버튼</Btn>
+      <Btn bg="orange">버튼</Btn> */}
+      {/* <BlackBox>
+        <YellowBtn>버튼</YellowBtn>
+      </BlackBox> */}
+
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width={"100%"}
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Detail;
