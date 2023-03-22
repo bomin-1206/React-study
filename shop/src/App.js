7576372cca98baedb8429d40b92d5c2585b97651
bomin-1206 <gbm4655@naver.com>
2023-03-22 17:52:57 +0900

    React_card(props)-0322

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,9 +5,15 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useState } from "react";
+import data from './data';
 
 // import bg from './img/bg.png';
 function App() {
+
+  let [shoes] = useState(data);
+  let [image] = useState(['https://codingapple1.github.io/shop/shoes1.jpg', 'https://codingapple1.github.io/shop/shoes2.jpg', 'https://codingapple1.github.io/shop/shoes3.jpg'])
+
   return (
     <div className="App">
       <Navbar bg="light" variant="light">
@@ -23,26 +29,28 @@ function App() {
       <div className="main-bg"></div>
       <Container>
         <Row>
-          <Col>
-            {/* <img src="https://codingapple1.github.io/shop/shoes1.jpg" width={'80%'}/> */}
-            <img src={process.env.PUBLIC_URL + '/logo192.png'} /> {/* public 폴더 안에 있는 이미지 쓰는 권장방식 */}
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width={'80%'}/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width={'80%'}/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
+          {
+            shoes.map(function(e, i) {
+              return (
+                <Card shoes={shoes} image={image} i={i}/>
+              )
+            })
+          }
         </Row>
     </Container>
     </div>
   );
+}
+
+function Card(props) {
+  return (
+    <Col>
+      <img src={props.image[props.i]} width={'80%'}/>
+      {/* <img src={process.env.PUBLIC_URL + '/logo192.png'} /> public 폴더 안에 있는 이미지 쓰는 권장방식 */}
+      <h4>{props.shoes[props.i].title}</h4>
+      <p>{props.shoes[props.i].content}</p>
+    </Col>
+  )
 }
 
 export default App;
