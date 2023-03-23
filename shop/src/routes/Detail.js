6983707d9deae3baaf4ad807167f4fce7d779f27
bomin-1206 <gbm4655@naver.com>
2023-03-23 15:06:49 +0900

    React_Lifecycle,useEffect-0323

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components';
 
@@ -5,7 +6,7 @@ import styled from 'styled-components';
 //   background: ${ props => props.bg };
 //   color: ${ props => props.bg == 'blue' ? 'white' : 'black' };
 //   padding: 10px;
-`
+// `
 // let NewBtn = styled.button(Btn)``
 
 // let BlackBox = styled.div`
@@ -13,12 +14,39 @@ import styled from 'styled-components';
 //   padding: 20px;
 // `
 
-function Detail(props) {
+// class Detail2 extends React.Component {
+//   componentDidMount() {
+
+//   }
+//   componentDidUpdate() {
+
+//   }
+//   componentWillUnmount() {
+
+//   }
+// }
+
+function Detail(props) {  
 
   let {id} = useParams();
   let 찾은상품 = props.shoes.find(function(x) {
     return x.id == id
   });
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+
+  useEffect(() => { // mount, update시 코드 실행해줌
+    // useEffect안에 있는 코드는 html 렌더링 후에 동작합니다.
+    // - 어려운 연산
+    // - 서버에서 데이터 가져오는 작업
+    // - 타이머 장착하는거
+    let a =setTimeout(() => { setAlert(false) }, 2000);
+    return () => { // useEffect 동작 전에 실행됨
+      // clearTimeout(a); // 타이머 제거해주는 함수
+    }
+  }, []) // []있어면 더 정확함
+  // useEffect 실행 조건 넣을 수 있는 곳은 []
+  // []안에 있는 state가 변할 때마다 실행해줌
 
   return (
     <div className="container">
@@ -29,6 +57,10 @@ function Detail(props) {
         <YellowBtn>버튼</YellowBtn>
       </BlackBox> */}
 
+      {
+        alert == true ? <div className="alert alert-warning">2초이내 구매시 할인</div> : null
+      }
+
       <div className="row">
         <div className="col-md-6">
           <img
@@ -37,6 +69,7 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6">
+          <input></input>
           <h4 className="pt-5">{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price}원</p>
