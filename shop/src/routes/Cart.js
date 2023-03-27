dd5df9ae050652affc0f498f3bc63e342af51da7
bomin-1206 <gbm4655@naver.com>
2023-03-27 09:12:54 +0900

    React_Redux-0327

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,23 +1,32 @@
 import { Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function Cart() {
+
+    let state = useSelector((state)=>{ return state }) // store에 있는 state를 모두 가져다씀
+    // let state = useSelector((state)=>{ return state.user }) // store에 있는 user만 가져다씀
+    console.log(state.stock)
+
   return (
     <div>
-      <Table>
+      <Table striped bordered hover>
         <thead>
           <tr>
             <th>#</th>
             <th>상품명</th>
             <th>수량</th>
-            <th>변경하기</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>안녕</td>
-            <td>안녕</td>
-            <td>안녕</td>
+            <td>{state.cart[0].id}</td>
+            <td>{state.cart[0].name}</td>
+            <td>{state.cart[0].count}</td>
+          </tr>
+          <tr>
+            <td>{state.cart[1].id}</td>
+            <td>{state.cart[1].name}</td>
+            <td>{state.cart[1].count}</td>
           </tr>
         </tbody>
       </Table>
